3d6fdac8647513a3de362873b8d57b81b55f7961
lqp1037951137 <lqp@liangqiaoping.cn>
2021-08-13 17:37:26 +0800

    1.0

--- src/view/demo3.vue
+++ src/view/demo3.vue
@@ -1,9 +1,33 @@
 <template>
-  <div><img v-for="img in imageList" v-lazy="img" :key="img"/></div>
+  <div>
+    <van-image
+      v-for="img in imageList"
+      lazy-load
+      :key="img"
+      :src="img"
+      width="100%"
+      height="auto"
+    ></van-image>
+  </div>
 </template>
 
 <script>
-export default {};
+import { Image } from "vant";
+export default {
+  components: {
+    "van-image": Image
+  },
+  data() {
+    return {
+      imageList: [
+        "https://img01.yzcdn.cn/vant/apple-1.jpg",
+        "https://img01.yzcdn.cn/vant/apple-2.jpg",
+        "https://img01.yzcdn.cn/vant/apple-3.jpg",
+        "https://img01.yzcdn.cn/vant/apple-4.jpg"
+      ]
+    };
+  }
+};
 </script>
 
 <style scoped></style>
