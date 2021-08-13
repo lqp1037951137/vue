3e301df7ed21fc555b8c8b22712d589a49ea23d1
lqp1037951137 <lqp@liangqiaoping.cn>
2021-08-13 17:19:26 +0800

    1.0

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-import Index from "@/view/demo2";
+import Index from "@/view/demo3";
 
 Vue.use(Router)
 
