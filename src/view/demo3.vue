d354fb2a3b519627bfd3f9feddb2b01a312d4b13
lqp1037951137 <lqp@liangqiaoping.cn>
2021-08-14 14:49:20 +0800

    8.14.14.49

--- src/view/demo3.vue
+++ src/view/demo3.vue
@@ -1,21 +1,29 @@
 <template>
   <div>
-    <van-image
-      v-for="img in imageList"
-      lazy-load
-      :key="img"
-      :src="img"
-      width="100%"
-      height="auto"
-    ></van-image>
+    <van-list
+      v-model="loading"
+      :finished="finished"
+      finished-text="没有更多了"
+      @load="onLoad"
+    >
+      <van-image
+        v-for="(img, index) in imageList"
+        lazy-load
+        :key="index"
+        :src="img"
+        width="100%"
+        height="auto"
+      ></van-image>
+    </van-list>
   </div>
 </template>
 
 <script>
-import { Image } from "vant";
+import { Image, List } from "vant";
 export default {
   components: {
-    "van-image": Image
+    "van-image": Image,
+    "van-list": List
   },
   data() {
     return {
@@ -23,9 +31,39 @@ export default {
         "https://img01.yzcdn.cn/vant/apple-1.jpg",
         "https://img01.yzcdn.cn/vant/apple-2.jpg",
         "https://img01.yzcdn.cn/vant/apple-3.jpg",
+        "https://img01.yzcdn.cn/vant/apple-4.jpg",
+        "https://img01.yzcdn.cn/vant/apple-1.jpg",
+        "https://img01.yzcdn.cn/vant/apple-2.jpg",
+        "https://img01.yzcdn.cn/vant/apple-3.jpg",
+        "https://img01.yzcdn.cn/vant/apple-4.jpg",
+        "https://img01.yzcdn.cn/vant/apple-1.jpg",
+        "https://img01.yzcdn.cn/vant/apple-2.jpg",
+        "https://img01.yzcdn.cn/vant/apple-3.jpg",
+        "https://img01.yzcdn.cn/vant/apple-4.jpg",
+        "https://img01.yzcdn.cn/vant/apple-1.jpg",
+        "https://img01.yzcdn.cn/vant/apple-2.jpg",
+        "https://img01.yzcdn.cn/vant/apple-3.jpg",
         "https://img01.yzcdn.cn/vant/apple-4.jpg"
-      ]
+      ],
+      finished: false,
+      loading: false
     };
+  },
+  methods: {
+    onLoad() {
+      setTimeout(() => {
+        this.imageList.push(
+          "https://img01.yzcdn.cn/vant/apple-1.jpg",
+          "https://img01.yzcdn.cn/vant/apple-2.jpg",
+          "https://img01.yzcdn.cn/vant/apple-3.jpg",
+          "https://img01.yzcdn.cn/vant/apple-4.jpg"
+        );
+        this.loading = false;
+        if (this.imageList.length >= 20) {
+          this.finished = true;
+        }
+      }, 2000);
+    }
   }
 };
 </script>
